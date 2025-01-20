5f9c9c7299f81b6c22db2322fd72a9666924722d
Rizwan727598 <rizwanislam369@gmail.com>
2025-01-20 13:39:47 +0600

    Add CRUD routes for Food collection

--- index.js
+++ index.js
@@ -203,3 +203,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
